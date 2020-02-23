1fb42c861b8ab00d740ccad26bafba6d4dc2ee1f
tornhill007 <tornhill1997@gmail.com>
2020-02-24 00:42:28 +0300

    init new

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 export const addComment = async (formData) => {
-    return await axios.post('http://localhost:3001/comments', formData);
+    return await axios.post('https://discussion-page-ak.herokuapp.com/', formData);
 };
 
 export const getCommentsList = async () => {
-    return await axios.get('http://localhost:3001/comments');
+    return await axios.get('https://discussion-page-ak.herokuapp.com/');
 };
 
 export const deleteComment = async (id) => {
-    return await axios.delete(`http://localhost:3001/comments/${id}`);
+    return await axios.delete(`https://discussion-page-ak.herokuapp.com/${id}`);
 };
 
 export const getCommentById = async (id) => {
-    return await axios.get(`http://localhost:3001/comments/${id}`);
+    return await axios.get(`https://discussion-page-ak.herokuapp.com/${id}`);
 };
 
 export const updateComment = async (id, data) => {
-    return await axios.patch(`http://localhost:3001/comments/${id}`, data);
+    return await axios.patch(`https://discussion-page-ak.herokuapp.com/${id}`, data);
 }
