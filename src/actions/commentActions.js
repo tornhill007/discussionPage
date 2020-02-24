ee7e73bfc308737661c2dbfd5f33341de1213846
tornhill007 <tornhill1997@gmail.com>
2020-02-24 14:55:52 +0300

    some changes

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 export const addComment = async (formData) => {
-    return await axios.post('https://discussion-page-ak.herokuapp.com/', formData);
+    return await axios.post('https://guarded-mountain-57996.herokuapp.com/comments', formData);
 };
 
 export const getCommentsList = async () => {
-    return await axios.get('https://discussion-page-ak.herokuapp.com/');
+    return await axios.get('https://guarded-mountain-57996.herokuapp.com/comments/');
 };
 
 export const deleteComment = async (id) => {
-    return await axios.delete(`https://discussion-page-ak.herokuapp.com/${id}`);
+    return await axios.delete(`https://guarded-mountain-57996.herokuapp.com/comments/${id}`);
 };
 
 export const getCommentById = async (id) => {
-    return await axios.get(`https://discussion-page-ak.herokuapp.com/${id}`);
+    return await axios.get(`https://guarded-mountain-57996.herokuapp.com/comments/${id}`);
 };
 
 export const updateComment = async (id, data) => {
-    return await axios.patch(`https://discussion-page-ak.herokuapp.com/${id}`, data);
+    return await axios.patch(`https://guarded-mountain-57996.herokuapp.com/comments/${id}`, data);
 }
